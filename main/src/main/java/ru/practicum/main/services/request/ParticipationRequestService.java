04ebbaa86280db248c1e7b9aaf4e5feb599232c6
Eldar <eldar.gai@yandex.ru>
2022-10-31 06:45:19 +0600

    refactor: add javadoc

--- main/src/main/java/ru/practicum/main/services/request/ParticipationRequestService.java
+++ main/src/main/java/ru/practicum/main/services/request/ParticipationRequestService.java
@@ -5,16 +5,54 @@ import ru.practicum.main.models.request.ParticipationRequest;
 import java.util.List;
 
 public interface ParticipationRequestService {
+
+    /**
+     * Adds a new ParticipationRequest.
+     * @param requesterId: ParticipationRequest identifier.
+     * @param eventId: Event identifier.
+     * @return ParticipationRequest object that was created.
+     */
     ParticipationRequest create(Long requesterId, Long eventId);
 
+    /**
+     * Setting cancel status for ParticipationRequest.
+     * @param userId User identifier.
+     * @param requestId ParticipationRequest identifier.
+     * @return ParticipationRequest with cancel status.
+     */
     ParticipationRequest cancel(Long userId, Long requestId);
 
+    /**
+     * Getting ParticipationRequest by requester id.
+     * @param requesterId requester identifier.
+     * @return list of ParticipationRequest.
+     */
     List<ParticipationRequest> getUserRequests(Long requesterId);
 
+    /**
+     * Getting ParticipationRequest by event initiator id and event id.
+     * @param userId event initiator identifier.
+     * @param eventId event identifier.
+     * @return list of ParticipationRequest.
+     */
     List<ParticipationRequest> getEventRequests(Long userId, Long eventId);
 
+    /**
+     * Setting confirm status for ParticipationRequest.
+     * @param userId event initiator identifier.
+     * @param eventId event identifier.
+     * @param reqId ParticipationRequest identifier.
+     * @return ParticipationRequest with confirm status.
+     */
     ParticipationRequest confirmRequest(Long userId, Long eventId, Long reqId);
 
+    /**
+     * Setting reject status for ParticipationRequest.
+     * @param userId event initiator identifier.
+     * @param eventId event identifier.
+     * @param reqId ParticipationRequest identifier.
+     * @return ParticipationRequest with reject status.
+     */
     ParticipationRequest rejectRequest(Long userId, Long eventId, Long reqId);
 
 }
--- main/src/main/java/ru/practicum/main/services/user/UserService.java
+++ main/src/main/java/ru/practicum/main/services/user/UserService.java
@@ -5,12 +5,30 @@ import ru.practicum.main.models.user.User;
 import java.util.List;
 
 public interface UserService {
+    /**
+     * Adds a new User and returns a User object that was created.
+     * @param: User object - new object.
+     */
     User create(User user);
 
+    /**
+     * Removing a User by user id.
+     * @param id User identifier.
+     */
     void delete(Long id);
 
+/**
+ * Getting a list of Users by params.
+ * @param ids - list of user identifiers.
+ * @param from integer value of the initial object on the page.
+ * @param size integer number of objects on page.
+ */
     List<User> getUsers(List<Long> ids, Integer from, Integer size);
 
+    /**
+     * Return a User by id.
+     * @param id user identifier.
+     */
     User getUser(Long id);
 
 }
